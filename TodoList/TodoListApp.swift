b74e176c04e2b5a570832df2c0b16ba4dc86f028
Vlad <boogovo@gmail.com>
2024-05-28 16:49:16 +0300

    Initial commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Vlad on 28/5/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
